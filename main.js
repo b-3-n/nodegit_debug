3fb0e209794d11cd37308fc0b4f739fa48f2321c
Benjamin Mularczyk <benjamin@deepcode.ai>
2018-05-14 18:52:59 +0200

    Add comments.

--- main.js
+++ main.js
@@ -14,10 +14,12 @@ lines.forEach(async line => {
   const [owner, repo, oid] = line.split('/');
 
   let gitRepo;
+  // I found using this queue data structure to be necessary to trigger the error.
   await FetchQueue.Enqueue(async () => {
     const cloneDir = `/tmp/clone/${owner}/${repo}`
     const gitURL = `https://github.com/${owner}/${repo}`;
     gitRepo = await NodeGit.Clone(gitURL, cloneDir);
   }); 
+  // I found this to be necessary to trigger the error.
   await gitRepo.getCommit(oid);
 });
